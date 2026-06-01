e09290ba0bc55efe5fd8c65278d2d5bde82bdbe5
wonnho71 <wonnho@nate.com>
2026-06-01 17:37:19 +0900

    add link to each title, set unique key in iternative statement

--- src/App.js
+++ src/App.js
@@ -15,42 +15,52 @@ function Header(props) {
   );
 }
 
-function Nav() {
+function Nav(props) {
+  const lists=[]
+  for(let k=0;k<props.topics.length;k++) {
+let t=props.topics[k];
+lists.push(<li key={t.id}><a href={`/read/${t.id}`}>{t.title}</a></li>)
+  }
   return (
     <div>
       <nav>
         <ul>
-          <li>
-            <a href="/about">Strategy</a>
-          </li>
-          <li>
-            <a href="/contact">Iran</a>
-          </li>
+       {lists}
         </ul>
       </nav>
     </div>
   );
 }
 
-function Article() {
+function Article(props) {
+ 
   return (
     <div>
       <article>
+        <h1>{props.title}</h1>
         <h2><a href="/https://en.wikipedia.org/wiki/2026_Iran_war">Iran War, 2026</a></h2>
         <p>Operation Epic Fury was a major joint military campaign launched by the United States and Israel against Iran and its regional proxies, which began on February 28, 2026, and officially concluded with a ceasefire in early May 2026.</p>
+      <blockquote>  
+        <p>{props.body} </p>
+        </blockquote>
       </article>
     </div>
   );
 }
 
 function App() {
+   const topics=[{id:1,title:'gold land',body:'Horror'},
+    {id:2,title:'Flight UA94',body:'Terrorism'},
+    {id:3,title:'Sakarika',body:'Gangstar'}   
+      ];
+
   return (
     <div>
       <Header title="Iran operations" />
 
-      <Nav />
+      <Nav topics={topics} />
 
-      <Article />
+      <Article title="April news, 2026" body="This is the story this month. " />
     </div>
   );
 }
